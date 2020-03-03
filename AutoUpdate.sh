6637431d6ff678f7ef1a4940e0ba2ab76e4e3477
Daniel Korsah <dkorsah15@aol.co.uk>
2020-03-03 18:23:07 +0000

    Clear console after a 1 day to avoid memory leaking

--- AutoUpdate.sh
+++ AutoUpdate.sh
@@ -7,37 +7,34 @@ NOMINAL='\e[38;5;14m' #light cyan
 NOCOLOUR='\e[39m' #no colour
 ERROR='\e[38;5;1m' #red
 
-#make strings for our paths
-REPO="[REPO LOCATION]"
-SITE="[WEBSITE LOCATION]"
+REPO="/home/pi/PersonalWebsite/"
+SITE="/var/www/"
 
-#move to git directory to start issuing git commands
+echo Git directory is $GITDIR
 cd $REPO
 
+i=0
+
 while true; do
-  
-  echo -e ${POLLING}Checking Git repo
-  echo -e =======================${NOCOLOUR}
+  i=$((i+1))
+  echo -e ${POLLING}Checking Git repo at $(date +%F-%T)
+  echo -e ===================================================${NOCOLOUR}
   git fetch
   HEADHASH=$(git rev-parse HEAD)
   UPSTREAMHASH=$(git rev-parse master@{upstream})
   BRANCH=$(git rev-parse --abbrev-ref HEAD)
-  #self explanatory, if this branch isn't master then quit
   if [ "$BRANCH" != "master" ]; then
     echo -e ${ERROR}Not on master. Aborting. ${NOCOLOUR}
     echo
    exit 0 
   fi
-  
-  #If hash of local repo doesn't match hash of remote then pull and apply changes
+
   if [ "$HEADHASH" != "$UPSTREAMHASH" ]; then
     echo -e ${UPDATE}Not up to date with origin. Pulling...${NOCOLOUR}
     git pull origin master
     echo -e ${UPDATE}Deleting old files in website directory...${NOCOLOUR}
-    #delete old files
     sudo rm -rf ${SITE}*
     echo -e ${UPDATE}Copying files to website directory...${NOCOLOUR}
-    #copy contents of repo to site desired directory
     #echo ${REPO}* ${SITE}
     sudo cp -r ${REPO}* ${SITE}
     echo -e ${UPDATE}Done! Updates applied.${NOCOLOUR}
@@ -45,7 +42,10 @@ while true; do
     echo -e ${NOMINAL}Current branch is up to date with origin/master. ${NOCOLOUR}
   fi
   
-  #wait sixty seconds before checking again if we're up to date
+  if[$i -eq 1440]; then
+    clear
+    i=0
+  fi
   sleep 60
-  
 done 
+
